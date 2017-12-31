5f5d1e4e45b276600b910534bf2d80cde12e1e7c
Amraddin Jamalli <amruldinjamalli@Amruldins-iMac.local>
2017-12-30 19:42:34 -0500

    Data persisted UserDefaults method

--- AJToDoList/ToDoListViewController.swift
+++ AJToDoList/ToDoListViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    var item = ""
 
     var itemArray = ["Call Him", "Call Bob", "BootCamp Meeting"]
+    
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String]
+        {
+            itemArray = items
+        }
         
         // Do any additional setup after loading the view, typically from a nib.
    
@@ -84,7 +89,12 @@ class ToDoListViewController: UITableViewController {
            
             self.itemArray.append(textField.text!)
             
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+        
+            
             self.tableView.reloadData()
+            
+            
             
             
             
